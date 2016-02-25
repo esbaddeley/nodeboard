5ce73e13aa2a93ffc741d6e77469e5260d95d296
esbaddeley <esbaddeley@gmail.com>
2016-02-25 11:33:27 +0000

    Setup testing framework and gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+grunt.initConfig({
+  pkg: grunt.file.readJSON('package.json'),
+  simplemocha: {
+    all: {
+      src: ['test/**/*.js']
+    }
+  }
+});
+
+// For this to work, you need to have run `npm install grunt-simple-mocha`
+grunt.loadNpmTasks('grunt-simple-mocha');
+
+// Add a default task. This is optional, of course :)
+grunt.registerTask('default', 'simplemocha');
+};
---
+++ test/features/indexFeature.js
@@ -0,0 +1,15 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../app');
+var expect = chai.expect();
+
+chai.use(chaiHttp);
+
+describe ('nodeboard', function(){
+
+  it ('serves up JSON with a welcome message ', function(){
+    
+  });
+
+
+});
